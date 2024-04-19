working full satellite control - Attitude controller tuned - Gravitational moment works, but not in sim - MPC tuned TODO: Ensure thrust units are correct, make controller low thrust

controllers.py
@@ -5,6 +5,9 @@ from scipy.spatial.transform import Rotation
 from helper_func import skew
 import sympy as sy
 
+def saturate(vec, limit=10):
+    return np.array([v if abs(v)<limit else 10*np.sign(v) for v in vec])
+
 def calc_orn_error(corn, dorn):
     error_num = dorn*(np.dot(corn, corn) - 1) + corn*(1 - np.dot(dorn, dorn)) + 2*skew(dorn)@corn
     error_den = 1 + np.dot(dorn, dorn)*np.dot(corn, corn) + 2*np.dot(dorn, corn)
@@ -36,12 +39,29 @@ def attitude_controller_v2(curr_orn, desired_orn, curr_omega, kp=10, kd=10):
     # corn = curr_orn.as_quat(canonical=True)
     # dorn = desired_orn.as_quat(canonical=True)
 
-    eorn = curr_orn*desired_orn.inv()
-    eorn = eorn.as_mrp()
+    eorn = desired_orn.inv()*curr_orn
+    # eorn = eorn.as_mrp()
+    eorn = eorn.as_quat()
     # eorn = curr_orn.as_quat(canonical=True)
     # eorn = dorn*quat_inv(corn)
+    torque = -kp*eorn[:3] - kd*curr_omega
 
-    return -kp*eorn[:3] - kd*curr_omega
+    return saturate(torque, limit=10)
+
+def attitude_controller_v3(curr_orn, desired_orn, curr_omega, int_args, kp=10, kd=10, ki=1):
+    # corn = curr_orn.as_quat(canonical=True)
+    # dorn = desired_orn.as_quat(canonical=True)
+    prev_quat, prev_int = int_args
+
+    eorn = desired_orn.inv()*curr_orn
+    # eorn = eorn.as_mrp()
+    eorn = eorn.as_quat()
+    integrated_quat = 1/2*(eorn[:3] - prev_quat) + prev_quat + prev_int
+    # eorn = curr_orn.as_quat(canonical=True)
+    # eorn = dorn*quat_inv(corn)
+    torque = -kp*eorn[:3] - kd*curr_omega + ki*integrated_quat
+
+    return torque, (eorn[:3], integrated_quat)
 
 def CW_model(n, dt):
     Phi_rr = np.array([[4-3*np.cos(n*dt)       , 0, 0           ],

dynamics.py
@@ -83,6 +83,11 @@ def attitude_wheels(t, y, M_w, I, I_wheels):
     wheel_dyn = wheel_dynamics(t, y[12:], -M_w, I_wheels)
     return np.concatenate((att_dyn, wheel_dyn))
 
+def attitude_wheels_moment(t, y, M_w, I, I_wheels, mu, r):
+    att_dyn = attitude_dynamics_w_moment(t, y[:12], M_w, I, mu, r)
+    wheel_dyn = wheel_dynamics(t, y[12:], -M_w, I_wheels)
+    return np.concatenate((att_dyn, wheel_dyn))
+
 def full_sat(t, y, args):
     F, M, m, I, mu, I_wheels = args
     rot_m = y[6:15].reshape(3, 3)
@@ -120,9 +125,9 @@ def main():
     y0_orbit = np.array([y10, 0, 0, 0, y2d0, 0])
 
     y0_rmat = np.eye(3).reshape(-1)
-    y0_w = np.array([0., 0., 0.])
+    y0_w = np.array([0.0, 0., 0.])
     y0_attitude = np.concatenate((y0_rmat, y0_w))
-    y0_wheels = np.array((0., 0., 0.))
+    y0_wheels = np.array((0., 0., 0., 0., 0., 0.))
 
     y0 = np.concatenate((y0_orbit, y0_rmat, y0_w, y0_wheels))
 
@@ -147,51 +152,20 @@ def main():
     # y_sol = sol.y.T
     ####
     # args_attitude = (M_w, I, mu, np.array([7_000., 0., 1]))
-
+    # # args_attitude = (M_w, I)
     # sol = solve_ivp(attitude_dynamics_w_moment, tspan, y0_attitude, t_eval=t_eval, args=args_attitude, **options)
     # y_sol = sol.y.T
-
-    # # tranform data to euler angles 
-    # angles = np.zeros((len(y_sol), 3))
-    # dets = np.zeros((len(y_sol)))
-    # for i,sol in enumerate(y_sol):
-    #     r_mat = sol[:9].reshape(3, 3)
-    #     print(nlg.det(r_mat))
-    #     r = Rotation.from_matrix(r_mat)
-    #     euler_angles = r.as_euler('xyz', degrees=True)
-    #     angles[i, :] = euler_angles
-    #     dets[i] = nlg.det(r_mat)
-
-    # fig, ax1 = plt.subplots(1, 3)
-    # ax1[0].plot(angles[:, 0])
-    # ax1[1].plot(angles[:, 1])
-    # ax1[2].plot(angles[:, 2])
-
-    # fig, ax2 = plt.subplots(1, 3)
-    # ax2[0].plot(y_sol[:, 9])
-    # ax2[1].plot(y_sol[:, 10])
-    # ax2[2].plot(y_sol[:, 11])
-
-    # fig, ax3 = plt.subplots()
-    # ax3.plot(dets)
-
-    #######
-    args = ((F, M_w, m, I, mu, I_wheels),)
-
-    sol = solve_ivp(full_sat_moment, tspan, y0, t_eval=t_eval, args=args, **options)
+    ####
+    args_attitude = (M_w, I, I_wheels, mu, np.array([7_000., 0., 1]))
+    sol = solve_ivp(attitude_wheels_moment, tspan, np.concatenate((y0_attitude, y0_wheels)), t_eval=t_eval, args=args_attitude, **options)
     y_sol = sol.y.T
 
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    plot_sphere(ax, r_e)
-    ax.plot(y_sol[:, 0], y_sol[:, 1], y_sol[:, 2]) 
-    ax.axis('equal')
-    
     # tranform data to euler angles 
     angles = np.zeros((len(y_sol), 3))
     dets = np.zeros((len(y_sol)))
     for i,sol in enumerate(y_sol):
-        r_mat = sol[6:15].reshape(3, 3)
+        r_mat = sol[:9].reshape(3, 3)
+        print(nlg.det(r_mat))
         r = Rotation.from_matrix(r_mat)
         euler_angles = r.as_euler('xyz', degrees=True)
         angles[i, :] = euler_angles
@@ -203,17 +177,52 @@ def main():
     ax1[2].plot(angles[:, 2])
 
     fig, ax2 = plt.subplots(1, 3)
-    ax2[0].plot(y_sol[:, 15])
-    ax2[1].plot(y_sol[:, 16])
-    ax2[2].plot(y_sol[:, 17])
+    ax2[0].plot(y_sol[:, 9])
+    ax2[1].plot(y_sol[:, 10])
+    ax2[2].plot(y_sol[:, 11])
 
-    fig, ax3 = plt.subplots()
-    ax3.plot(dets)
+    # fig, ax3 = plt.subplots()
+    # ax3.plot(dets)
 
-    fig, ax4 = plt.subplots(1, 3)
-    ax4[0].plot(y_sol[:, 18])
-    ax4[1].plot(y_sol[:, 19])
-    ax4[2].plot(y_sol[:, 20])
+    #######
+    # args = ((F, M_w, m, I, mu, I_wheels),)
+
+    # sol = solve_ivp(full_sat_moment, tspan, y0, t_eval=t_eval, args=args, **options)
+    # y_sol = sol.y.T
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(projection='3d')
+    # plot_sphere(ax, r_e)
+    # ax.plot(y_sol[:, 0], y_sol[:, 1], y_sol[:, 2]) 
+    # ax.axis('equal')
+    
+    # # tranform data to euler angles 
+    # angles = np.zeros((len(y_sol), 3))
+    # dets = np.zeros((len(y_sol)))
+    # for i,sol in enumerate(y_sol):
+    #     r_mat = sol[6:15].reshape(3, 3)
+    #     r = Rotation.from_matrix(r_mat)
+    #     euler_angles = r.as_euler('xyz', degrees=True)
+    #     angles[i, :] = euler_angles
+    #     dets[i] = nlg.det(r_mat)
+
+    # fig, ax1 = plt.subplots(1, 3)
+    # ax1[0].plot(angles[:, 0])
+    # ax1[1].plot(angles[:, 1])
+    # ax1[2].plot(angles[:, 2])
+
+    # fig, ax2 = plt.subplots(1, 3)
+    # ax2[0].plot(y_sol[:, 15])
+    # ax2[1].plot(y_sol[:, 16])
+    # ax2[2].plot(y_sol[:, 17])
+
+    # fig, ax3 = plt.subplots()
+    # ax3.plot(dets)
+
+    # fig, ax4 = plt.subplots(1, 3)
+    # ax4[0].plot(y_sol[:, 18])
+    # ax4[1].plot(y_sol[:, 19])
+    # ax4[2].plot(y_sol[:, 20])
 
     plt.show()
 

helper_func.py
@@ -18,6 +18,13 @@ class Arrow3D(FancyArrowPatch):
 
         return np.min(zs)
 
+def normalize(rotmat):
+    rotmat = rotmat.reshape(3, 3)
+    for i in range(3):
+        rotmat[:, i] = rotmat[:, i]/nlg.norm(rotmat[:, i])
+    return rotmat.flatten()
+
+
 def skew(vec):
     return np.array([[0      , -vec[2], vec[1] ],
                      [vec[2] , 0      , -vec[0]],
@@ -31,7 +38,7 @@ def vec2att(vec, att):
     vec = vec / nlg.norm(vec)
     v3 = np.cross(vec, att[:, 1])
     v2 = np.cross(v3, vec)
-    new_mat = np.array([vec, v2, v3])
+    new_mat = np.array([vec, v2/nlg.norm(v2), v3/nlg.norm(v3)]).T
     return new_mat
 
 def visualize_orn(rot_mat, ax, c='b'):

simulation.py
@@ -3,14 +3,15 @@ import numpy.linalg as nlg
 import matplotlib.pyplot as plt
 from scipy.spatial.transform import Rotation
 import dynamics as dyn
-from helper_func import vec2att, visualize_orn, save_data
+from helper_func import vec2att, visualize_orn, save_data, Arrow3D, normalize
 from scipy.integrate import solve_ivp
 from scipy.linalg import solve_discrete_are, block_diag
-from controllers import CW_model, attitude_controller_v2
+from controllers import attitude_controller, CW_model, attitude_controller_v2, attitude_controller_v3
 from pyMPC.pyMPC.mpc import MPCController
 from tqdm import tqdm
 from functools import partial
 from dataclasses import dataclass
+
 
 @dataclass
 class RunData:
@@ -49,27 +50,38 @@ def satellite_sim(sat1_state, sat2_state, tf, dt, moment=False):
     ## Initialize Orbit Controller
     # Controller Weights
     Q = np.diag([1, 1, 1, 1, 1, 1])
-    R = np.diag([1e6, 1e6, 1e6])
+    R = np.diag([1e4, 1e4, 1e4])
     Rd = np.diag([1e6, 1e6, 1e6])
+    # R = np.diag([1e-2, 1e-2, 1e-2])
+    # Rd = np.diag([1e4, 1e4, 1e4])
     # B matrix describing effect of control
     B = np.block([[np.zeros((3, 3))],
-                  [np.eye(3)*dt]])
+                  [np.eye(3)*dt/m2]])
+    # B = np.block([[np.zeros((3, 3))],
+    #               [np.eye(3)*dt/m2/1000]])
     # Find Clohessy Wiltshire model
     at = nlg.norm(sat1_state[:3]) # Assumes circular orbit
     n = np.sqrt(MU/at**3) # This value should be non-dimensional for distance (m and km) so no conversion either way is necesarry as long as they are consistant
     CW_mat = CW_model(n, dt)
     # MPC
     dx = sat2_state[:6] - sat1_state[:6] # Initialization
-    umin = -10*np.ones(3) # minimum thrust
-    umax = 10*np.ones(3) # maximum thrust
-    Np = int(tf/dt * (0.1)) # Horizon is a fraction of simulation steps
-    K = K = MPCController(CW_mat, B, Np=Np, x0=dx,
+    umin = -1*np.ones(3) # minimum thrust
+    umax = 1*np.ones(3) # maximum thrust
+    Np = int(tf/dt * (0.3)) # Horizon is a fraction of simulation steps
+    K = MPCController(CW_mat, B, Np=Np, x0=dx,
                   Qx=Q, Qu=R,QDu=Rd,
                   umin=umin,umax=umax)
     K.setup()
 
+    # Initialize Initial Attitude
+    thrust_vec = K.output()
+    current_attitude = Rotation.from_matrix(sat2_state[6:15].reshape(3, 3))
+    desired_attitude = vec2att(thrust_vec, current_attitude.as_matrix())
+    sat2_state[6:15] = desired_attitude.flatten()
+
     ## Initialize Attitude Controller
-    attitude_controller = partial(attitude_controller_v2, kp=200, kd=20)
+    attitude_controller = partial(attitude_controller_v2, kp=100, kd=10)
+    # attitude_controller = partial(attitude_controller_v2, kp=np.array((100, 100, 500)), kd=np.array((10, 10, 50)))
 
     ## Pre-sim Initializations
     sat1_states = []
@@ -78,6 +90,7 @@ def satellite_sim(sat1_state, sat2_state, tf, dt, moment=False):
     thrust_vecs = []
     torques = []
     # args =((thrust, torques, sim_args['m'], sim_args['I'], sim_args['mu'], sim_args['I_wheels']),)
+    int_args = (np.array((0, 0, 0)), np.array((0, 0, 0)))
 
     ## Sim Loop
     for i in tqdm(range(int(tf/dt))):
@@ -93,6 +106,11 @@ def satellite_sim(sat1_state, sat2_state, tf, dt, moment=False):
         torque = attitude_controller(current_attitude, 
                                     desired_attitude, 
                                     curr_omega=sat2_state[15:18])
+        # torque, int_args = attitude_controller_v3(current_attitude, 
+        #                             desired_attitude, 
+        #                             curr_omega=sat2_state[15:18], 
+        #                             int_args=int_args,
+        #                             kp=200, kd=20, ki=1)
 
         # Integrate Sat1
         sat1_sol = solve_ivp(sat1_dynamics, [0, dt], sat1_state, **options)
@@ -102,13 +120,15 @@ def satellite_sim(sat1_state, sat2_state, tf, dt, moment=False):
         sat2_args = ((thrust, torque, m2, I, MU, I_wheels),)
         sat2_sol = solve_ivp(sat2_dynamics, [0, dt], sat2_state, args=sat2_args, **options)
         sat2_state = sat2_sol.y[:, -1]
+        sat2_state[6:15] = normalize(sat2_state[6:15]) # Renormalize rotation matrix
 
         # Save States
         sat1_states.append(sat1_state)
         sat2_states.append(sat2_state)
         thrust_vecs.append(thrust_vec)
         torques.append(torque)
-        relative_dist.append(sat1_state[:3] - sat2_state[:3])
+        dx = sat2_state[:6] - sat1_state[:6]
+        relative_dist.append(dx)
 
     ## Save Data
     sat1_states = np.array(sat1_states)
@@ -145,15 +165,17 @@ def satellite_sim(sat1_state, sat2_state, tf, dt, moment=False):
     ax2.plot(us[:, 1], 'b')
     ax2.plot(us[:, 2], 'b')
 
-    # actual_controls = np.array([*states[i, 6:9] for i in range(len(states))])
-
     plt.show()
 
 def attitude_sim(init_state, tf, dt, attitude_controller):
     # Init state = [orn_matrix, omega] in R^12
     J = np.diag((1, 2, 3)) #np.eye(3)
-    desired_vector = np.array([1, 0.25, 0.25])
-    
+    desired_vector = np.array([1, 1, 1])
+    r_e = 6_378  # km
+    yx0 = r_e + 408.773
+    r = np.array([yx0, 0, 0])
+    r = np.array([7_000., 0., 7_000.])
+
     # Sim loop
     options = {
         'rtol' : 1e-12,
@@ -162,6 +184,7 @@ def attitude_sim(init_state, tf, dt, attitude_controller):
     wheel_state = np.array([0, 0, 0])
     init_state = np.concatenate((init_state, wheel_state))
 
+    int_args = (np.array((0, 0, 0)), np.array((0, 0, 0)))
     states = [init_state]
     torques_l = []
     for i in tqdm(range(int(tf/dt))):
@@ -178,18 +201,23 @@ def attitude_sim(init_state, tf, dt, attitude_controller):
         torques = attitude_controller_v2(current_attitude, 
                                     desired_attitude, 
                                     curr_omega=init_state[9:12],
-                                    kp=200, kd=20)
-        # torques = attitude_controller_v2(current_attitude, 
+                                    kp=100, kd=10)
+        # torques, int_args = attitude_controller_v3(current_attitude, 
         #                             desired_attitude, 
-        #                             curr_omega=init_state[9:12],
-        #                             kp=100, kd=10)
+        #                             curr_omega=init_state[9:12], 
+        #                             int_args=int_args,
+        #                             kp=200, kd=20, ki=1)
         # torques = np.array([0.0, 0.0, 0.0])
         # torques[2] = -torques[2]
         torques_l.append(torques)
 
         # Integrator
-        args_attitude = (torques, np.diag(J), np.diag(J))
-        sol = solve_ivp(dyn.attitude_wheels, [0, dt], init_state, args=args_attitude, **options)
+        args_attitude = (torques, np.diag(J), np.diag(J), MU, r)
+        sol = solve_ivp(dyn.attitude_wheels_moment, [0, dt], init_state, args=args_attitude, **options)
+        # args_attitude = (torques, np.diag(J), np.diag(J))
+        # sol = solve_ivp(dyn.attitude_wheels, [0, dt], init_state, args=args_attitude, **options)
+        
+        
         states.append(sol.y[:, -1])
         # args_attitude = (np.diag(J), desired_attitude)
         # sol = solve_ivp(dyn.attitude_dynamics_no_moment_controller, [0, dt], init_state, args=args_attitude, **options)
@@ -204,7 +232,7 @@ def attitude_sim(init_state, tf, dt, attitude_controller):
     errors = [Rotation.from_matrix(states[i,:9].reshape(3, 3)@desired_orn_inv) for i in range(len(states))]
     # errors = [Rotation.from_matrix(states[i,:9].reshape(3, 3)) for i in range(len(states))]
 
-    error_euler = np.array([r.as_euler('XYZ') for r in errors])
+    error_euler = np.array([r.as_euler('xyz') for r in errors])
     fig, ax = plt.subplots()
     ax.plot(error_euler[:, 0])
     ax.plot(error_euler[:, 1])
@@ -216,6 +244,14 @@ def attitude_sim(init_state, tf, dt, attitude_controller):
     ax2.plot(torques_l[:, 1])
     ax2.plot(torques_l[:, 2])
 
+    omegas = states[:, 9:12]
+    fig3, ax3 = plt.subplots()
+    ax3.plot(omegas[:, 0])
+    ax3.plot(omegas[:, 1])
+    ax3.plot(omegas[:, 2])
+
+
+
     init_orn = np.eye(3) # init_state[:9].reshape(3, 3)
     final_orn = states[-1, :9].reshape(3, 3)
     fig1, ax1 = plt.subplots(subplot_kw={"projection": "3d"})
@@ -226,6 +262,11 @@ def attitude_sim(init_state, tf, dt, attitude_controller):
     visualize_orn(init_orn, ax1, c='b')
     visualize_orn(desired_orn, ax1, c='g')
     visualize_orn(final_orn, ax1, c='r')
+    v = desired_vector
+    a = Arrow3D([0, v[0]], [0, v[1]], 
+                [0, v[2]], mutation_scale=20, 
+                lw=3, arrowstyle="-|>", color='m')
+    ax1.add_artist(a)
 
     plt.show()
 
@@ -304,9 +345,9 @@ def orbit_control_sat_sim_MPC(sat1_state, sat2_state, tf, dt):
 
     # Init Controller
     Q = np.diag([1, 1, 1, 1, 1, 1])
-    R = np.diag([1e6, 1e6, 1e6])
+    R = np.diag([1e4, 1e4, 1e4])
     B = np.block([[np.zeros((3, 3))],
-                  [np.eye(3)*dt]])
+                  [np.eye(3)*dt/m2/1000]])
     at = nlg.norm(sat1_state[:3]) # Assumes circular orbit
     n = np.sqrt(mu/at**3) # This value should be non-dimensional for distance (m and km) so no conversion either way is necesarry as long as they are consistant
     CW_mat = CW_model(n, dt)
@@ -363,13 +404,13 @@ def orbit_control_sat_sim_MPC(sat1_state, sat2_state, tf, dt):
 
 
 def main():
-    # r = Rotation.from_euler('XYZ', [0, 0, 0.1])
+    # r = Rotation.from_euler('xyz', [1, 1, 1], degrees=True)
     # init_rot = r.as_matrix().flatten()# np.eye(3).flatten()
     # init_w = np.array((0.0, 0.0, 0.0)) # np.zeros((3,))
     # init_state = np.concatenate((init_rot, init_w))
     # attitude_sim(
     #     init_state=init_state,
-    #     tf = 3, 
+    #     tf = 300, 
     #     dt = 0.1,
     #     attitude_controller=attitude_controller
     # )
@@ -378,7 +419,7 @@ def main():
     # mu = 398600.4418
     # y10 = r_e + 408.773
     # y2d0 = np.sqrt(mu/y10)
-    # R = Rotation.from_euler('XYZ', [0, 0, 45], degrees=True)
+    # R = Rotation.from_euler('xyz', [0, 0, 5], degrees=True)
     # sat1_state = np.array([y10, 0, 0, 0, y2d0, 0])
     # sat2_state = block_diag(R.as_matrix(),R.as_matrix()) @ np.array([y10, 0, 0, 0, y2d0, 0])
     # orbit_control_sat_sim_MPC(sat1_state, sat2_state, tf=1000, dt=0.1)
@@ -388,17 +429,17 @@ def main():
     mu = 398600.4418
     y10 = r_e + 408.773
     y2d0 = np.sqrt(mu/y10)
-    R = Rotation.from_euler('XYZ', [0, 0, 5], degrees=True)
+    R = Rotation.from_euler('xyz', [0, 0, 0.1], degrees=True)
     sat1_state = np.array([y10, 0, 0, 0, y2d0, 0])
     sat2_state_pos = block_diag(R.as_matrix(),R.as_matrix()) @ np.array([y10, 0, 0, 0, y2d0, 0])
     
     # Attitude States
-    r = Rotation.from_euler('XYZ', [0, 0, 0.1])
+    r = Rotation.from_euler('xyz', [0, 0, 0.1])
     init_rot = r.as_matrix().flatten() # np.eye(3).flatten()
     init_w = np.array((0.0, 0.0, 0.0)) # np.zeros((3,))
     sat2_state = np.concatenate((sat2_state_pos, init_rot, init_w))
 
-    satellite_sim(sat1_state, sat2_state, tf=10, dt=.1)
+    satellite_sim(sat1_state, sat2_state, tf=150, dt=.1)
 
 if __name__ == "__main__":
     main()